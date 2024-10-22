59be68fe9d3ea3d7259052216c32fb44b5b83245
sandeep kumar <sandeep.kumar@gumtreegroup.com.au>
2024-10-22 21:40:41 +1100

    Refactor Code

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-//import expenses from expenses.json
+
 const fs = require('fs');
 let date = new Date();
 let expenses =  require('./financials/expenses.json') // Importing expenses.json file
 let people = require('./financials/people.json') // Importing people.json file
 let currentState = require('./financials/currentState.json') // Importing currentState.json file
 let count = expenses.length; // Getting the length of the expenses array
-//iterate through the expenses array
 
-if(currentState.processed.expenseId === expenses[count-1].id) {
-    console.log("No new expenses to process");
-} else {
+let generateDateStamp = function() {
+    return date.toString().replace(/\s/g, '');
+}
+
+let updateFunds = function() {
     for (let i = 0; i < count; i++) {
         //add each expense to the total
         let share = expenses[i].amount / expenses[i].applies.length;
@@ -22,23 +23,28 @@ if(currentState.processed.expenseId === expenses[count-1].id) {
             people[person].fund = updatedFund;
         }
     }
-    currentState.processed.expenseId = expenses[count-1].id;
 }
 
+let updatePeopleFunds = function() {
+    let data = JSON.stringify(people);
+    //copy people file to archive directory
+    fs.copyFileSync('./financials/people.json', './financials/archive/people' + generateDateStamp() + '.json');
+    fs.writeFileSync('./financials/people.json', data);
+}
 
-console.log("People after expenses are: ");
-console.log(people);
+let generateUpdatedState = function() {
+    let currentStateData = JSON.stringify(currentState);
+    fs.copyFileSync('./financials/currentState.json', './financials/archive/currentState' + generateDateStamp() + '.json');
+    fs.writeFileSync('./financials/currentState.json', currentStateData);
+}
 
-//write people object to json file
-let data = JSON.stringify(people);
-//copy people file to archive directory
-fs.copyFileSync('./financials/people.json', './financials/archive/people' + date + '.json');
-fs.writeFileSync('./financials/people.json', data);
-//console.log the updated people object
-let currentStateData = JSON.stringify(currentState);
-//add time stamp to the file name currentState.json
-//copy currentState file to archive directory
-fs.copyFileSync('./financials/currentState.json', './financials/archive/currentState' + date + '.json');
-fs.writeFileSync('./financials/currentState.json', currentStateData);
+//iterate through the expenses array
+if(currentState.processed.expenseId === expenses[count-1].id) {
+    console.log("No new expenses to process");
+} else {
+    updateFunds()
+    currentState.processed.expenseId = expenses[count-1].id;
+}
+updatePeopleFunds();
+generateUpdatedState();
 
-//assign current date time to a variable
